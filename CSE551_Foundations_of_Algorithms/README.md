ea26073b599a1e77900e6a4dc7eac0e1cb41639b
Mishel Paul <misheljyothis@gmail.com>
2021-08-25 19:44:29 +0530

    CSE551: Added stable marriage problem GS solution v1

---
+++ CSE551_Foundations_of_Algorithms/README.md
@@ -0,0 +1,6 @@
+## Classwork for course CSE 551 - Foundations of Algorithms @ ASU
+
+In this area, I will try to implement algorithms covered in class. Not trying to end up with the most optimized solution for python (trying to keep this as generic as possible to be easily implemented in other languages). Implementations will be directly based on textbook algorithms. Area is otherwise unmonitored. 
+Most (or all) of this will be in Python 3.
+
+Anything posted here is my original work and is provided "as is" without any warranty or liability on my part. Message me on LinkedIn if you have any feedback/questions.
---
+++ CSE551_Foundations_of_Algorithms/Stable_matching/Marriage_problem/man_proposes_to_women/gale_shapley_minimal.py
@@ -0,0 +1,86 @@
+#######################################################
+#  Gale-Shapley algorithm for Stable Marriage Problem #
+#  -------------------------------------------------- #
+# * Time complexity : O(n^2)                          #
+# * Basic algorithm with hardcoded values             #
+#                                                     #
+# - Python3 implementation, Mishel Paul, 08/25/2021   #
+#######################################################
+
+import random
+
+## Number of men & women (in pairs)
+num_pairs = 3
+
+## List of people
+men_list = ['Xavier', 'Yancey', 'Zeus']
+women_list = ['Amy', 'Bertha', 'Claire']
+
+## Sample preference tables
+men_pref   = [[0,1,2],
+              [1,0,2],
+              [0,1,2]]
+women_pref = [[1,0,2],
+              [0,1,2],
+              [0,1,2]]
+
+women_inv_pref = []
+for woman_pref in women_pref:
+    inv_pref = [-1]*num_pairs
+    for i in range(num_pairs):
+        inv_pref[woman_pref[i]] = i
+    women_inv_pref.append(inv_pref)
+
+## Queue for free men; initialized to everyone free
+free_men = list(range(num_pairs))
+random.shuffle(free_men)
+
+## Array for number of women proposed to by a man
+proposal_count = [0]*num_pairs
+
+## Matching result data (initialized to -1 for unmatched since indexing starts from 0)
+husband = [-1]*num_pairs
+wife = [-1]*num_pairs
+
+## Gale-Shapley / propose-and-reject algorithm
+
+# algorithm stable_matching is
+#     Initialize m belongs to M and w belongs W to free
+#     while there exists free man m who has a woman w to propose to do
+#         w := first woman on m's list to whom m has not yet proposed
+#         if there exists some pair (m', w) then
+#             if w prefers m to m' then
+#                 m' becomes free
+#                 (m, w) become engaged
+#             end if
+#         else
+#             (m, w) become engaged
+#         end if
+#     repeat
+#
+# Source: https://en.wikipedia.org/wiki/Gale%E2%80%93Shapley_algorithm
+
+while(len(free_men)!=0):
+    m = free_men.pop(0)
+    print("Matching man {0}".format(men_list[m]))
+    wife[m] = -1
+    if proposal_count[m] > num_pairs:
+        print(f'Man {m} has exhausted max number of proposals. GS somehow failed (0_0) or there is something wrong with the implementation.')
+        break
+    while(wife[m]==-1 and proposal_count[m]<=num_pairs):
+        w = men_pref[m][proposal_count[m]]
+        print("Proposing to {0}".format(women_list[w]))
+        m_prime = husband[w]
+        if m_prime == -1:
+            husband[w] = m
+            wife[m] = w
+        elif m_prime != -1 and women_inv_pref[w][m_prime]>women_inv_pref[w][m]:
+            husband[w] = m
+            wife[m] = w
+            wife[m_prime] = -1
+            free_men.append(m_prime)
+        proposal_count[m] += 1
+
+print("\nFinal stable perfect matching:")
+for i in range(num_pairs):
+    print(men_list[i], women_list[wife[i]])
